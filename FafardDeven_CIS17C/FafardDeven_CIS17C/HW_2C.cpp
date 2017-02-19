cf44537d6810b5389443ed6893a62822d7f26054
Deven Fafard <deven.marie.fafard@gmail.com>
2017-02-18 17:17:03 -0800

    HW2C [UNFINISHED]
    Initial upload of Assignment 2C. It's like... 80% done.

---
+++ FafardDeven_CIS17C/FafardDeven_CIS17C/HW_2C.cpp
@@ -0,0 +1,165 @@
+/*
+Attached: HW_2ABC
+========================================
+Project: HW_2C.cpp
+========================================
+Programmer: Deven Fafard
+Class: CIS-17C
+========================================
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+using namespace std;
+
+//Function Prototypes
+void getGrades(char[][3], const int ROW, const int COL);
+void displayGrades(char arr[][3], const int ROW, const int COL);
+void calcGPA(double gpas[5], char arr[][3], const int ROW, const int COL);
+void calcAvgGPA(double avgs[3], char arr[][3], const int ROW, const int COL);
+
+int main()
+{
+	const int CLASS_SIZE = 5, CLASS_AMT = 3;
+	char grades[CLASS_SIZE][CLASS_AMT];
+	double gpas[5] = { 0 };
+	double avgGpas[3] = { 0 };
+	
+	getGrades(grades, CLASS_SIZE, CLASS_AMT);
+	displayGrades(grades, CLASS_SIZE, CLASS_AMT);
+	calcGPA(gpas, grades, CLASS_SIZE, CLASS_AMT);
+	calcAvgGPA(avgGpas, grades, CLASS_SIZE, CLASS_AMT);
+
+
+	return 0;
+}
+
+void getGrades(char arr[][3], const int ROW, const int COL)
+{
+	ifstream gradeSheet;
+	gradeSheet.open("Grades.txt");
+	char temp;
+	
+	if (gradeSheet.is_open())
+	{
+		for (int i = 0; i < ROW; i++)
+		{
+			for (int j = 0; j < COL; j++)
+			{
+				gradeSheet >> temp;
+				arr[i][j] = temp;
+			}
+		}
+	}
+	else
+	{
+		cout << "*** UNABLE TO OPEN FILE ***" << endl;
+	}
+	
+	gradeSheet.close();
+}
+
+
+void displayGrades(char arr[][3], const int ROW, const int COL)
+{
+	cout << "------------- GRADES -------------" << endl
+		<< "STUDENT   " << "ENGLISH   " << "HISTORY   " << "MATH      " << endl;
+
+	for (int i = 0; i < ROW; i++)
+	{
+		cout << "#" << i + 1;
+		for (int j = 0; j < COL; j++)
+		{
+			cout << setw(10) << arr[i][j];
+		}
+
+		cout << endl;
+	}
+}
+
+void calcGPA(double gpas[5], char arr[][3], const int ROW, const int COL)
+{
+	double sum = 0;
+	
+	for (int i = 0; i < ROW; i++)
+	{
+		sum = 0;
+		for (int j = 0; j < COL; j++)
+		{
+			switch (arr[i][j])
+			{
+				case 'A':
+					sum += 4;
+					break;
+				case 'B':
+					sum += 3;
+					break;
+				case 'C':
+					sum += 2;
+					break;
+				case 'D':
+					sum += 1;
+					break;
+				case 'F':
+					break;
+			}			
+		}
+
+		gpas[i] = sum / 3;
+	}
+
+	cout << "-------------- GPAS --------------" << endl
+		<< "STUDENT   " << "GPA       " << endl;
+
+	for (int i = 0; i < ROW; i++)
+	{
+		cout << "#" << i + 1;
+		cout << setw(10) << fixed << setprecision(2) << gpas[i] << endl;
+	}
+}
+
+void calcAvgGPA(double avgs[3], char arr[][3], const int ROW, const int COL)
+{
+	double sum = 0;
+
+	for (int i = 0; i < ROW; i++)
+	{
+		for (int j = 0; i < COL; i++)
+		{
+			switch (arr[i][j])
+			{
+				case 'A':
+					avgs[j] += 4;
+					break;
+				case 'B':
+					avgs[j] += 3;
+					break;
+				case 'C':
+					avgs[j] += 2;
+					break;
+				case 'D':
+					avgs[j] += 1;
+					break;
+				case 'F':
+					break;
+			}
+		}
+		
+		for (int i = 0; i < COL; i++)
+		{
+			avgs[i] = avgs[i] / 3;
+		}
+		
+	}
+	cout << "----------  AVERAGE GPAS ----------" << endl
+		<< "ENGLISH   " << "HISTORY   " << "MATH      " << endl;
+
+	for (int i = 0; i < COL; i++)
+	{
+		cout << setw(10) << fixed << setprecision(2) << avgs[i];
+	}
+
+	cout << endl << endl;
+}
